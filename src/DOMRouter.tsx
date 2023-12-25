75844a617262698ff82343429970610f9bb95717
Marcus Rognes <marcus@rognes.guru>
2023-12-25 11:48:39 +0100

    Added event handler on popstate

--- src/DOMRouter.tsx
+++ src/DOMRouter.tsx
@@ -1,14 +1,26 @@
 import { Router } from './Router';
-import { ReactNode, useState } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 
 
 export function DOMRouter(props: { children: ReactNode }) {
 	const [location, _setLocation] = useState(window.location.pathname);
 
-	function setLocation(newLocation: string){
+	function setLocation(newLocation: string) {
 		_setLocation(newLocation);
-		window.history.pushState(null, "", newLocation);
+		window.history.pushState(null, '', newLocation);
 	}
+
+	useEffect(() => {
+		function onPopstate() {
+			_setLocation(window.location.pathname);
+		}
+
+		window.addEventListener('popstate', onPopstate);
+		return () => {
+			window.removeEventListener('popstate', onPopstate);
+		};
+
+	}, [false]);
 
 	return <Router location={location} setLocation={setLocation}>
 		{props.children}
