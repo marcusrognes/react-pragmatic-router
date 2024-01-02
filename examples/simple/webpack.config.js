75f4a837dd7fa44b6860d492ef96b07ebdbcaa6f
Marcus Rognes <marcus@rognes.guru>
2024-01-02 13:45:53 +0100

    Force added config.js file

---
+++ examples/simple/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    devtool: 'inline-source-map',
+    entry: './examples/simple/index.tsx',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './examples/simple/index.html',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                loader: 'ts-loader',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    },
+    devServer: {
+        compress: true,
+        historyApiFallback: true,
+        port: 3000,
+        client: {
+            overlay: {
+                errors: true,
+                warnings: false,
+            },
+        },
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+            'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+        },
+    },
+};
